365fa7d61034ffd3231bb18c59d635bb3edfbf02
kate <k7bart@gmail.com>
2024-04-17 15:31:36 +0300

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,7 @@
 import Logo from "./Logo";
 import { Link } from "react-router-dom";
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 
 const linkStyle = {
     opasity: "0.5",
@@ -8,10 +10,25 @@ const linkStyle = {
 const Footer = () => {
     return (
         <div className="footer">
-            <div>
+            <div className="container">
                 <Logo />
+
+                <Link
+                    to="https://www.instagram.com/catherine_pastry/"
+                    target="_blank"
+                    className="link"
+                >
+                    <FaInstagram />
+                    @nique.
+                </Link>
+                <Link to="" target="_blank" className="link">
+                    <FaFacebook />
+                    nique.
+                </Link>
+
+                <p>By Kateryna Bartienieva</p>
             </div>
-            <div className="footer__link-container">
+            <div className="container">
                 <Link to="/menu" className="link" style={linkStyle}>
                     Menu
                 </Link>
