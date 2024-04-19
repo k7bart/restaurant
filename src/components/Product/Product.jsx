e0f4104d5d2b6aa91006b56e6dc45acf7a49f304
kate <k7bart@gmail.com>
2024-04-19 14:58:51 +0300

    updated product

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import "./Product.scss";
 import menu from "../Menu/menu";
+import Carrousel from "../Carrousel/Carrousel";
 
 const Product = () => {
     const { category } = useParams();
@@ -10,7 +11,15 @@ const Product = () => {
         .find((c) => c.name === category)
         .dishes.find((p) => p.id === productId);
 
-    return <div className="product">{product.name}</div>;
+    return (
+        <div className="product">
+            <h3>{product.name}</h3>
+
+            <div className="carrousel-container">
+                <Carrousel photos={product.photos} dots={true} />
+            </div>
+        </div>
+    );
 };
 
 export default Product;
