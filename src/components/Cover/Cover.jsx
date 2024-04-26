7caff8e9704d09c37eef1aa0d2f7cc6741cbea0e
kate <k7bart@gmail.com>
2024-04-26 18:01:01 +0300

    worked on responsiveness

--- src/components/Cover/Cover.jsx
+++ src/components/Cover/Cover.jsx
@@ -1,8 +1,14 @@
+import useWindowWidth from "../../hooks/useWindowWidth";
 import Logo from "../Logo/Logo";
 import NavBar from "../NavBar/NavBar";
 import "./Cover.scss";
 
 function Cover({ subtitle, title, backgroundImage }) {
+    const windowWidth = useWindowWidth();
+
+    const logo = windowWidth > 820 && <Logo />;
+    const navbar = windowWidth > 820 && <NavBar />;
+
     return (
         <div className="section">
             <div className="cover-container">
@@ -12,12 +18,12 @@ function Cover({ subtitle, title, backgroundImage }) {
                 >
                     <div className="filter"></div>
                     <div className="content">
-                        <Logo />
+                        {logo}
                         <div className="titles-container">
                             <h2 className="subtitle">{subtitle}</h2>
                             <h1 className="title">{title}</h1>
                         </div>
-                        <NavBar />
+                        {navbar}
                     </div>
                 </div>
             </div>
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,5 +1,14 @@
+import ResponsiveNavbar from "./ResponsiveNavBar/ResponsiveNavBar";
+
 const Page = ({ children }) => {
-    return <div className="page">{children}</div>;
+    return (
+        <div className="page">
+            <header>
+                <ResponsiveNavbar />
+            </header>
+            <body>{children}</body>
+        </div>
+    );
 };
 
 export default Page;
