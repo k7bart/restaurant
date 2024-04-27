2761bf58c0afc7dc8cb95657f58fd3df98a87dc7
kate <k7bart@gmail.com>
2024-04-27 21:29:39 +0300

    rewrite using PageWithCover component

--- src/components/Reservation/Reservation.jsx
+++ src/components/Reservation/Reservation.jsx
@@ -1,35 +1,38 @@
-import Page from "../Page";
+import PageWithCover from "../PageWithCover/PageWithCover";
 import Cover from "../Cover/Cover";
 import ReservationForm from "./ReservationForm";
 import Footer from "../Footer/Footer";
 import image from "../../assets/cake.jpeg";
 
 function Reservation() {
-    return (
-        <Page>
-            <Cover
-                subtitle={"Book a table"}
-                title={"Reservation"}
-                backgroundImage={image}
-            />
-            <div className="section">
-                <div className="content">
-                    <div>
-                        <h3>Book a table</h3>
-                        <p className="text">
-                            In order to align with the relaxed and comfortable
-                            ambiance of our dining setting, we encourage a
-                            casual attire.
-                        </p>
-                    </div>
-
-                    <ReservationForm />
-
-                    <Footer />
-                </div>
-            </div>
-        </Page>
+    const cover = (
+        <Cover
+            subtitle={"Book a table"}
+            title={"Reservation"}
+            backgroundImage={image}
+        />
     );
+
+    const section = (
+        <div className="section">
+            <div className="content">
+                <div>
+                    <h3>Book a table</h3>
+                    <p className="text">
+                        In order to align with the relaxed and comfortable
+                        ambiance of our dining setting, we encourage a casual
+                        attire.
+                    </p>
+                </div>
+
+                <ReservationForm />
+
+                <Footer />
+            </div>
+        </div>
+    );
+
+    return <PageWithCover cover={cover} section={section}></PageWithCover>;
 }
 
 export default Reservation;
