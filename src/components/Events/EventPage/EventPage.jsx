5b4583e654e251f6860eb6d9eb83e639c53d63eb
kate <k7bart@gmail.com>
2024-04-29 09:15:08 +0300

    rewrite EventPage using PageWithCover

--- src/components/Events/EventPage/EventPage.jsx
+++ src/components/Events/EventPage/EventPage.jsx
@@ -1,98 +1,105 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import Page from "../../Page.jsx";
+import { useParams, NavLink } from "react-router-dom";
+import PageWithCover from "../../PageWithCover/PageWithCover.jsx";
 import Cover from "../../Cover/Cover";
 import Footer from "../../Footer/Footer.jsx";
-import events from "../events.js";
-import stuff from "../stuff.js";
-import "./EventPage.scss";
-import { NavLink } from "react-router-dom";
-
 import SpecialGuest from "./SpecialGuest/SpecialGuest";
+import { events, staff } from "../../../state.js";
+import "./EventPage.scss";
 
 const EventPage = () => {
     const { eventId } = useParams();
     const event = events.find((e) => e.id === eventId);
-    const guest = stuff.find((person) => person.name === event.specialGuest);
+    const guest = staff.find((person) => person.name === event.specialGuest);
 
-    return (
-        <Page>
+    const cover = (
+        <>
             <Cover
                 subtitle={event.subtitle}
                 title={event.title}
                 backgroundImage={event.photo}
             />
-            <div className="section event-page">
-                <div className="content">
-                    <nav className="navigation">
-                        {events.map((event) => (
-                            <NavLink
-                                to={`/events/${event.id}`}
-                                className="link"
-                                key={event.id}
-                            >
-                                {event.title}
-                            </NavLink>
-                        ))}
-                    </nav>
-                    <div>
-                        <header>
-                            <h3>Reserve Your Spot</h3>
-                            <p className="text">
-                                Lorem ipsum dolor sit amet, consectetur
-                                adipiscing elit, sed do eiusmod tempor
-                                incididunt ut labore et dolore magna aliqua.
-                                Porttitor massa id neque aliquam.
-                            </p>
-                        </header>
+        </>
+    );
+    const section = (
+        <section className="section event-page">
+            <nav className="navigation">
+                {events.map((event) => (
+                    <NavLink
+                        to={`/events/${event.id}`}
+                        className="link"
+                        key={event.id}
+                    >
+                        {event.title}
+                    </NavLink>
+                ))}
+            </nav>
+            <div className="content">
+                <div>
+                    <header>
+                        <h3>Reserve Your Spot</h3>
+                        <p className="text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua. Porttitor massa id neque
+                            aliquam.
+                        </p>
+                    </header>
 
-                        <div className="reservation">
-                            <button className="small-color-button">
-                                BOOK A SPOT
-                            </button>
-                            <h4>${event.price}</h4>
-                        </div>
-
-                        <section className="subsection">
-                            <h3>Details</h3>
-                            <div className="details">
-                                <div>
-                                    <h4>Date</h4>
-                                    <p>{event.date}</p>
-                                </div>
-
-                                {guest && (
-                                    <div>
-                                        <h4>Special guest</h4>
-                                        <div className="special-guest">
-                                            <img src={guest.photo} alt="" />
-                                            <p>{guest.name}</p>
-                                        </div>
-                                    </div>
-                                )}
-
-                                <div>
-                                    <h4>Location</h4>
-                                    <p>Lviv</p>
-                                </div>
-                                <div>
-                                    <h4>Language</h4>
-                                    <p>{event.language}</p>
-                                </div>
-                            </div>
-                        </section>
-
-                        <section className="subsection">
-                            <h3>Menu</h3>
-                        </section>
-
-                        {guest && <SpecialGuest guest={guest} />}
+                    <div className="reservation">
+                        <button className="small-color-button">
+                            BOOK A SPOT
+                        </button>
+                        <h4>${event.price}</h4>
                     </div>
 
-                    <Footer />
+                    <section className="subsection">
+                        <h3>Details</h3>
+                        <div className="details">
+                            <div>
+                                <h4>Date</h4>
+                                <p>{event.date}</p>
+                            </div>
+
+                            {guest && (
+                                <div>
+                                    <h4>Special guest</h4>
+                                    <div className="special-guest">
+                                        <img src={guest.photo} alt="" />
+                                        <p>{guest.name}</p>
+                                    </div>
+                                </div>
+                            )}
+
+                            <div>
+                                <h4>Location</h4>
+                                <p>Lviv</p>
+                            </div>
+                            <div>
+                                <h4>Language</h4>
+                                <p>{event.language}</p>
+                            </div>
+                        </div>
+                    </section>
+
+                    <section className="subsection">
+                        <h3>Menu</h3>
+                    </section>
+
+                    {guest && <SpecialGuest guest={guest} />}
                 </div>
+
+                <Footer />
             </div>
-        </Page>
+        </section>
+    );
+
+    return (
+        <PageWithCover
+            cover={cover}
+            section={section}
+            addLogo={true}
+            addNavBar={true}
+        />
     );
 };
 
