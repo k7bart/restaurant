3d542c4ab648790f6f8f92c1bba1095aa51eed68
kate <k7bart@gmail.com>
2024-04-29 09:17:59 +0300

    move EventsPage to Pages folder

---
+++ src/components/Pages/EventsPage/EventsPage.jsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+import { events } from "../../../state";
+import Logo from "../../Logo/Logo";
+import NavBar from "../../NavBar/NavBar";
+import Cover from "../../Cover/Cover";
+import Carrousel from "../../Carrousel/Carrousel";
+import "./EventsPage.scss";
+
+const EventsPage = () => {
+    const e = events.map((event) => (
+        <Link to={`/events/${event.id}`} key={event.id} className="container">
+            <Cover
+                subtitle={event.subtitle}
+                title={event.title}
+                backgroundImage={event.photo}
+                text={event.date}
+            />
+        </Link>
+    ));
+
+    const props = {
+        content: e,
+        num: 3,
+        dots: false,
+        slideShow: false,
+    };
+
+    return (
+        <div className="events-page">
+            <Logo />
+            <Carrousel {...props} />
+            <NavBar />
+        </div>
+    );
+};
+
+export default EventsPage;
