0d83000f5be33d239f6805695a56cda349128a8b
kate <k7bart@gmail.com>
2024-05-04 15:32:57 +0300

    create NumInput component

---
+++ src/components/NumInput/NumInput.jsx
@@ -0,0 +1,39 @@
+import { FaPlus } from "react-icons/fa";
+import { FaMinus } from "react-icons/fa";
+import "./NumInput.scss";
+
+const NumInput = ({ amount, onChange }) => {
+    const handleIncrement = () => {
+        onChange(amount + 1);
+    };
+    const handleDecrement = () => {
+        if (amount === 1) return;
+        onChange(amount - 1);
+    };
+
+    return (
+        <div className="numInput">
+            <button
+                className="product-count less-product"
+                onClick={handleDecrement}
+            >
+                <FaMinus />
+            </button>
+            <input
+                value={amount}
+                maxLength="3"
+                type="text"
+                inputMode="numeric"
+                readOnly // to prevent manual editing
+            />
+            <button
+                className="product-count more-product"
+                onClick={handleIncrement}
+            >
+                <FaPlus />
+            </button>
+        </div>
+    );
+};
+
+export default NumInput;
