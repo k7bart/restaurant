79a5d00e6f422a0c03bbc9ad7b37d8325501b235
kate <k7bart@gmail.com>
2024-05-06 17:39:31 +0300

    enhance layout and functionality for Product component

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,35 +1,90 @@
-import { useParams } from "react-router-dom";
-import "./Product.scss";
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { addProduct, updateProductAmount } from "../../store";
+
 import { menu } from "../../state";
-import Logo from "../Logo/Logo";
-import Carrousel from "../Carrousel/Carrousel";
+import { IoClose } from "react-icons/io5";
+import NumInput from "../NumInput/NumInput";
+import Nutrients from "./Nutrients";
+import ProductCarrousel from "./ProductCarrousel";
+import "./Product.scss";
+
+// is it even legal?
+const CloseButton = ({ to }) => {
+    return (
+        <Link to={to}>
+            <button className="close-button">
+                <IoClose />
+            </button>
+        </Link>
+    );
+};
 
 const Product = () => {
     const { category } = useParams();
     const { productId } = useParams();
-
     const product = menu
         .find((c) => c.name === category)
         .dishes.find((p) => p.id === productId);
 
-    const i = product.photos.map((photo, i) => (
-        <img key={i} className="image" src={photo} />
-    ));
-
-    const props = {
-        content: i,
-        num: 1,
-        dots: true,
-        slideShow: false,
+    // amount
+    const [amount, setAmount] = useState(1);
+    const handleAmountChange = (newAmount) => {
+        setAmount(newAmount);
     };
+
+    // store
+    const dispatch = useDispatch();
+    const cart = useSelector((state) => state.cart);
+    const handleAddProduct = () => {
+        const productInCart = cart.find((product) => product.id === productId);
+
+        if (productInCart) {
+            const productId = productInCart.id;
+            const newAmount = productInCart.amount + amount;
+            dispatch(updateProductAmount({ productId, newAmount }));
+            return;
+        }
+
+        dispatch(addProduct({ ...product, amount, category }));
+    };
+
+    // totalPrice
+    const [totalPrice, setTotalPrice] = useState(product.price);
+    useEffect(() => {
+        setTotalPrice(product.price * amount);
+    }, [product.price, amount]);
 
     return (
         <div className="product">
-            <Logo />
+            <CloseButton to="/menu" />
+            <ProductCarrousel product={product} />
+            <div className="description">
+                <h3>{product.name}</h3>
+                <h4>{product.ingredients.join(", ")}</h4>
+                <Nutrients nutrients={product.nutrients} />
+                <div className="prices-container">
+                    <div className="container">
+                        <h4>Amount:</h4>
+                        <NumInput
+                            amount={amount}
+                            onChange={handleAmountChange}
+                        />
+                    </div>
+                    <div className="container">
+                        <h4>Total: $</h4>
+                        <h3 className="price">{totalPrice}</h3>
+                    </div>
+                </div>
 
-            <h3>{product.name}</h3>
-
-            <Carrousel {...props} />
+                <button
+                    className="small-color-button"
+                    onClick={handleAddProduct}
+                >
+                    Add to cart
+                </button>
+            </div>
         </div>
     );
 };
