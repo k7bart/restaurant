bf63793c35ea0fe8765471344446b8c531cf6a10
kate <k7bart@gmail.com>
2024-05-06 17:45:41 +0300

    rewrite TableReservationPage with Section component

--- src/components/Pages/TableReservationPage/TableReservationPage.jsx
+++ src/components/Pages/TableReservationPage/TableReservationPage.jsx
@@ -1,34 +1,27 @@
 import Cover from "../../Cover/Cover";
-import Footer from "../../Footer/Footer";
 import PageWithCover from "../../PageWithCover/PageWithCover";
 import ReservationForm from "./TableReservationForm";
+import Section from "../../Section/Section";
 import image from "../../../assets/cake.jpeg";
 
 function TableReservationPage() {
+    const header = {
+        title: "Book a table",
+        text: "In order to align with the relaxed and comfortable ambiance of our dining setting, we encourage a casual attire.",
+    };
+
+    const section = (
+        <Section header={header}>
+            <ReservationForm />
+        </Section>
+    );
+
     const cover = (
         <Cover
             subtitle={"Book a table"}
             title={"Reservation"}
             backgroundImage={image}
         />
-    );
-
-    const section = (
-        <section className="section">
-            <div className="content">
-                <header>
-                    <h3>Book a table</h3>
-                    <p className="text">
-                        In order to align with the relaxed and comfortable
-                        ambiance of our dining setting, we encourage a casual
-                        attire.
-                    </p>
-                </header>
-
-                <ReservationForm />
-                <Footer />
-            </div>
-        </section>
     );
 
     return (
