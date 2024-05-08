f9319d87326bd6d6f2ad755ba04f02a83b3eb1f1
kate <k7bart@gmail.com>
2024-05-08 09:05:03 +0300

    rewrite using TwoSectionsPage, CoverSection

--- src/components/Pages/EventPage/EventPage.jsx
+++ src/components/Pages/EventPage/EventPage.jsx
@@ -1,27 +1,25 @@
 import { Outlet, useParams } from "react-router-dom";
 import { events } from "../../../state.js";
 import Cover from "../../Cover/Cover.jsx";
-import PageWithCover from "../../PageWithCover/PageWithCover.jsx";
+import CoverSection from "../../CoverSection";
+import TwoSectionsPage from "../../TwoSectionsPage";
 import "./EventPage.scss";
 
 const EventPage = () => {
     const { eventId } = useParams();
     const event = events.find((e) => e.id === eventId);
-    const cover = (
-        <Cover
-            subtitle={event.subtitle}
-            title={event.title}
-            backgroundImage={event.photo}
-        />
-    );
 
     return (
-        <PageWithCover
-            cover={cover}
-            section={<Outlet />}
-            addLogo={true}
-            addNavBar={true}
-        />
+        <TwoSectionsPage>
+            <CoverSection>
+                <Cover
+                    subtitle={event.subtitle}
+                    title={event.title}
+                    backgroundImage={event.photo}
+                />
+            </CoverSection>
+            <Outlet />
+        </TwoSectionsPage>
     );
 };
 
