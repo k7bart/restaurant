ca206b07b379ad28e89e4f8ab093e303690d487e
kate <k7bart@gmail.com>
2024-05-09 12:10:54 +0300

    create UserProfilePage

---
+++ src/components/Pages/UserProfilePage/UserProfilePage.jsx
@@ -0,0 +1,49 @@
+import { useSelector } from "react-redux";
+import AccordionItem from "./AccordionItem";
+import Cover from "../../Cover/Cover";
+import CoverSection from "../../CoverSection";
+import OrderHistory from "./OrderHistory";
+import TwoSectionsPage from "../../TwoSectionsPage";
+import ContentSection from "../../ContentSection";
+import image from "../../../assets/covers/table-in-cafe.jpeg";
+import "./UserProfilePage.scss";
+
+const UserProfilePage = () => {
+    const header = {
+        title: "Personal profile",
+        text: "Manage your personal information and preferences here.",
+    };
+    const userName = useSelector((state) => state.user.name);
+
+    // Order history
+    // Personal data
+    // Delivery addresses
+    // Referral link and promo code
+    // Message
+
+    const Accordion = () => {
+        return (
+            <div className="accordion">
+                <OrderHistory />
+                <AccordionItem title="Personal data" />
+            </div>
+        );
+    };
+
+    return (
+        <TwoSectionsPage>
+            <CoverSection>
+                <Cover
+                    subtitle="Your presence brightens our day"
+                    title={"Hello, " + userName}
+                    backgroundImage={image}
+                />
+            </CoverSection>
+            <ContentSection header={header}>
+                <Accordion />
+            </ContentSection>
+        </TwoSectionsPage>
+    );
+};
+
+export default UserProfilePage;
