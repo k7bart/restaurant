0b40c1c0541c07e15d224e23c1b86ae011495ab8
kate <k7bart@gmail.com>
2024-05-20 17:45:40 +0300

    rewrite EventReservation using ContentSection

--- src/components/Pages/EventPage/EventReservation/EventReservation.jsx
+++ src/components/Pages/EventPage/EventReservation/EventReservation.jsx
@@ -1,28 +1,21 @@
 import { useParams } from "react-router-dom";
 import { events } from "../../../../state.js";
-
+import ContentSection from "../../../ContentSection.jsx";
 import EventReservationForm from "./EventReservationForm.jsx";
-import Footer from "../../../Footer/Footer.jsx";
 
 const EventReservation = () => {
     const { eventId } = useParams();
     const event = events.find((e) => e.id === eventId);
-    return (
-        <section className="section">
-            <div className="content">
-                <header>
-                    <h3>Signing up for the {event.title}</h3>
-                    <p className="text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Porttitor massa id neque aliquam.
-                    </p>
-                </header>
 
-                <EventReservationForm />
-                <Footer />
-            </div>
-        </section>
+    return (
+        <ContentSection
+            header={{
+                title: "Signing up for the " + event.title,
+                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore et dolore magna aliqua. Porttitor massa id neque aliquam.",
+            }}
+        >
+            <EventReservationForm />
+        </ContentSection>
     );
 };
 
