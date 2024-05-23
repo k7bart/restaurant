f10e6b370cbdc4fd879b8415208b16055966fc9a
kate <k7bart@gmail.com>
2024-05-23 12:18:49 +0300

    improve form validation

--- src/components/Pages/RegistrationPage/RegistrationForm.jsx
+++ src/components/Pages/RegistrationPage/RegistrationForm.jsx
@@ -1,16 +1,31 @@
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { capitalizeFirstLetter } from "../../../utils/stringUtils";
 import * as yup from "yup";
 
 import ContactInput from "../../Inputs/ContactInput";
-import NameInput from "../../Inputs/NameInput";
 
-const reservationSchema = yup.object({
-    name: yup.string().required(),
-    email: yup.string().email().required(),
-    phone: yup.string().required(),
-    password: yup.string().required(),
-    confirmPassword: yup.string().required(),
+const registrationSchema = yup.object({
+    name: yup.string().required("Please provide your name"),
+    surname: yup.string().optional(),
+    email: yup
+        .string()
+        .email("Please provide a valid email address")
+        .optional(),
+    // todo: add phone validation
+    phone: yup.string().required("Please provide your phone number"),
+    password: yup
+        .string()
+        .required("Please provide your password")
+        .test(
+            "is-valid-length",
+            "The password must be at least 8 characters long",
+            (value) => !value || value.length >= 8
+        ),
+    confirmPassword: yup
+        .string()
+        .oneOf([yup.ref("password"), null], "Passwords must match")
+        .required("Please confirm your password"),
 });
 
 const RegistrationForm = () => {
@@ -19,14 +34,36 @@ const RegistrationForm = () => {
         handleSubmit,
         formState: { errors },
     } = useForm({
-        resolver: yupResolver(reservationSchema),
+        resolver: yupResolver(registrationSchema),
     });
 
-    const onSubmit = (data) => console.log(data);
+    const onSubmit = (data) => {
+        const formattedData = {
+            ...data,
+            name: capitalizeFirstLetter(data.name),
+            surname: capitalizeFirstLetter(data.surname),
+        };
+        console.log(formattedData);
+    };
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <NameInput register={register} errors={errors} />
+            <div>
+                <label>
+                    <p>Name</p>
+                    <input {...register("name")} type="text" />
+                    {errors.name && (
+                        <p className="error">{errors.name.message}</p>
+                    )}
+                </label>
+                <label>
+                    <p>Surname</p>
+                    <input {...register("surname")} type="text" />
+                    {errors.surname && (
+                        <p className="error">{errors.surname.message}</p>
+                    )}
+                </label>
+            </div>
             <ContactInput register={register} errors={errors} />
 
             <div>
@@ -34,15 +71,17 @@ const RegistrationForm = () => {
                     <p>Password</p>
                     <input {...register("password")} type="password" />
                     {errors.password && (
-                        <p className="error">Please provide your password</p>
+                        <p className="error">{errors.password.message}</p>
                     )}
                 </label>
 
                 <label>
                     <p>Confirm password</p>
                     <input {...register("confirmPassword")} type="password" />
-                    {errors.password && (
-                        <p className="error">Please provide your password</p>
+                    {errors.confirmPassword && (
+                        <p className="error">
+                            {errors.confirmPassword.message}
+                        </p>
                     )}
                 </label>
             </div>
@@ -55,7 +94,7 @@ const RegistrationForm = () => {
                 </label>
             </div>
 
-            <button className="submit" type="sumbit">
+            <button className="submit" type="submit">
                 Login
             </button>
         </form>
