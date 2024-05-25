3ec7da82ab2e5bfe4110f5aa1a664fadcef6fd04
kate <k7bart@gmail.com>
2024-05-25 22:46:05 +0300

    correct capitalizeFirstLetters

--- src/utils/stringUtils.js
+++ src/utils/stringUtils.js
@@ -1,5 +1,10 @@
-export const capitalizeFirstLetter = (string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+export const capitalizeFirstLetters = (str) => {
+    return str
+        .split(" ")
+        .map(
+            (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+        )
+        .join(" ");
 };
 
 export const removeSpaces = (str) => {
