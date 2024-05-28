9bb8f414a8e719636718bca1b7369f4526d79383
kate <k7bart@gmail.com>
2024-05-28 17:56:32 +0300

    add removeAddress reducer, add currentAddress and addresses to state,

--- src/store/slices/userSlice.js
+++ src/store/slices/userSlice.js
@@ -13,13 +13,13 @@ const userSlice = createSlice({
                 id: 49068,
                 date: "19:45 08/03/2024", // change to Date
                 price: 48,
-                address: "Lviv, Chornovola st, 87/1",
+                address: "Lviv, Chornovola st, 85/7",
             },
             {
                 id: 84740,
                 date: "19:55 03/01/2024", // change to Date
                 price: 64,
-                address: "Lviv, Chornovola st, 87/1",
+                address: "Khmelnytskyi, Chornovola st, 87/1",
             },
             {
                 id: 72897,
@@ -27,6 +27,12 @@ const userSlice = createSlice({
                 price: 28,
                 address: "Lviv, Chornovola st, 87/1",
             },
+        ],
+        currentAddress: "Lviv, Chornovola st, 87/1",
+        addresses: [
+            "Lviv, Chornovola st, 85/7",
+            "Khmelnytskyi, Chornovola st, 87/1",
+            "Lviv, Chornovola st, 87/1",
         ],
     },
     reducers: {
@@ -38,8 +44,13 @@ const userSlice = createSlice({
             state.phone = phone;
             state.birthday = birthday;
         },
+        removeAddress(state, action) {
+            state.addresses = state.addresses.filter(
+                (address) => address !== action.payload
+            );
+        },
     },
 });
 
-export const { updateUserData } = userSlice.actions;
+export const { updateUserData, removeAddress } = userSlice.actions;
 export const userReducer = userSlice.reducer;
