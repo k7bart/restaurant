2576dfcfa10728f83c44691dbbb6367d73970563
kate <k7bart@gmail.com>
2024-05-29 16:37:37 +0300

    create RemoveAddressPopup component

---
+++ src/components/Pages/UserProfilePage/RemoveAddressPopup.jsx
@@ -0,0 +1,34 @@
+import Popup from "../../Popup";
+
+import { useDispatch } from "react-redux";
+import { removeAddress } from "../../../store";
+
+const RemoveAddressPopup = ({ closePopup, address }) => {
+    const dispatch = useDispatch();
+    const handleAddressRemove = (address) => {
+        dispatch(removeAddress(address));
+    };
+
+    return (
+        <Popup closePopup={closePopup}>
+            <h4>Do you want to delete the address?</h4>
+            <p className="large">{address}</p>
+            <div className="container">
+                <button
+                    className="small transparent"
+                    onClick={() => {
+                        handleAddressRemove(address);
+                        closePopup();
+                    }}
+                >
+                    Yes
+                </button>
+                <button className="small color" onClick={closePopup}>
+                    No
+                </button>
+            </div>
+        </Popup>
+    );
+};
+
+export default RemoveAddressPopup;
