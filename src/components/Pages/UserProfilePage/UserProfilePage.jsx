41a2e7c88009f625194b2dd31c7bcfdfd21d7a28
kate <k7bart@gmail.com>
2024-06-07 11:30:08 +0300

    add DeliveryAddresses component to ProfilePage

--- src/components/Pages/UserProfilePage/UserProfilePage.jsx
+++ src/components/Pages/UserProfilePage/UserProfilePage.jsx
@@ -2,29 +2,28 @@ import { useSelector } from "react-redux";
 import AccordionItem from "./AccordionItem";
 import Cover from "../../Cover/Cover";
 import CoverSection from "../../CoverSection";
+import DeliveryAddresses from "./DeliveryAddresses/DeliveryAddresses";
 import OrderHistory from "./OrderHistory";
 import PersonalData from "./PersonalData";
 import TwoSectionsPage from "../../TwoSectionsPage";
 import ContentSection from "../../ContentSection";
 import image from "../../../assets/covers/table-in-cafe.jpeg";
-import "./UserProfilePage.scss";
 
 const UserProfilePage = () => {
     const header = {
         title: "Personal profile",
         text: "Manage your personal information and preferences here.",
     };
-    const userName = useSelector((state) => state.user.name);
-
-    // Referral link and promo code
-    // Message
+    const user = useSelector((state) => state.user);
 
     const Accordion = () => {
         return (
             <div className="accordion">
-                <OrderHistory />
-                <PersonalData />
-                <AccordionItem title="Delivery addresses" />
+                <OrderHistory user={user} />
+                <PersonalData user={user} />
+                <DeliveryAddresses user={user} />
+                {/* <AccordionItem title="Referral link and promo code" />
+                <AccordionItem title="Message" /> */}
             </div>
         );
     };
@@ -34,7 +33,7 @@ const UserProfilePage = () => {
             <CoverSection>
                 <Cover
                     subtitle="Your presence brightens our day"
-                    title={"Hello, " + userName}
+                    title={"Hello, " + user.name}
                     backgroundImage={image}
                 />
             </CoverSection>
