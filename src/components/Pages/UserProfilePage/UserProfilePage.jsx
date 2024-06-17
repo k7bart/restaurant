215ddef486df856ed6458e3cc4d8502c72985a48
kate <k7bart@gmail.com>
2024-06-17 14:31:25 +0300

    add TableReservations

--- src/components/Pages/UserProfilePage/UserProfilePage.jsx
+++ src/components/Pages/UserProfilePage/UserProfilePage.jsx
@@ -1,13 +1,14 @@
 import { useSelector } from "react-redux";
 import AccordionItem from "./AccordionItem";
+import ContentSection from "../../ContentSection";
 import Cover from "../../Cover/Cover";
 import CoverSection from "../../CoverSection";
 import DeliveryAddresses from "./DeliveryAddresses/DeliveryAddresses";
 import OrderHistory from "./OrderHistory";
 import PersonalData from "./PersonalData";
 import Referral from "./Referral";
+import Reservations from "./Reservations";
 import TwoSectionsPage from "../../TwoSectionsPage";
-import ContentSection from "../../ContentSection";
 
 const UserProfilePage = () => {
     const header = {
@@ -20,6 +21,7 @@ const UserProfilePage = () => {
         return (
             <div className="accordion">
                 <OrderHistory user={user} />
+                <Reservations reservations={user.reservations} />
                 <PersonalData user={user} />
                 <DeliveryAddresses user={user} />
                 <Referral id={user.id} />
@@ -29,7 +31,7 @@ const UserProfilePage = () => {
     };
 
     return (
-        <TwoSectionsPage title="Profile">
+        <TwoSectionsPage title="Profile" className="profile-page">
             <CoverSection>
                 <Cover
                     subtitle="Your presence brightens our day"
