1952fade53a98229e2101db81a750da37f19b0d2
kate <k7bart@gmail.com>
2024-06-18 14:09:37 +0300

    split to NumberOfAdultsInput and NumberOfChildrenInput, rewrite using Input component

---
+++ src/components/Inputs/NumberOfAdultsInput.jsx
@@ -0,0 +1,9 @@
+import Input from "./Input";
+
+const NumberOfAdultsInput = (props) => {
+    return (
+        <Input fieldName="numberOfAdults" label="Number of Adults" {...props} />
+    );
+};
+
+export default NumberOfAdultsInput;
