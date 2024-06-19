add71bfb0451a99c9e87feb4be3a28dea342cbf8
kate <k7bart@gmail.com>
2024-06-19 15:08:01 +0300

    move HeaderNavigation styles to index.scss

--- src/components/HeaderNavigation/HeaderNavigation.jsx
+++ src/components/HeaderNavigation/HeaderNavigation.jsx
@@ -1,12 +1,11 @@
-import { IoMenu } from "react-icons/io5";
 import Logo from "../Logo/Logo";
+import { IoMenu } from "react-icons/io5";
 import { FaUserCircle } from "react-icons/fa";
-import "./HeaderNavigation.scss";
 
 function HeaderNavigation() {
     return (
         <header>
-            <nav className="header-navigation">
+            <nav>
                 <IoMenu />
                 <Logo />
                 <FaUserCircle />
