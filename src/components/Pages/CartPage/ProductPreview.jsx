d670d860e64ff628a3c588c4021353a23eb8d884
Kateryna Bartienieva <k7bart@gmail.com>
2024-07-29 15:07:27 +0300

    refactor ProductPreview

--- src/components/Pages/CartPage/ProductPreview.jsx
+++ src/components/Pages/CartPage/ProductPreview.jsx
@@ -6,49 +6,38 @@ import { IoClose } from "react-icons/io5";
 import NumInput from "../../Inputs/NumInput/NumInput";
 
 const ProductPreview = ({ product }) => {
-    const productLink = `/menu/${product.category}/${product.id}`;
-    const imageSrc = product.photos[0];
-    const price = (
-        <div className="price">
-            {product.onSale && (
-                <span className="sale">
-                    <h4>${product.oldPrice}</h4>
-                </span>
-            )}
-            <h4>${product.price}</h4>
-        </div>
-    );
+    const {
+        amount: initialAmount,
+        category,
+        id,
+        name,
+        photos,
+        price,
+    } = product;
 
-    const [amount, setAmount] = useState(product.amount);
+    const [amount, setAmount] = useState(initialAmount);
+
     const dispatch = useDispatch();
+
     const handleAmountChange = (newAmount) => {
         setAmount(newAmount);
-        dispatch(updateProductAmount({ productId: product.id, newAmount }));
+        dispatch(updateProductAmount({ productId: id, newAmount }));
     };
     const handleProductRemove = () => {
-        dispatch(removeProduct(product.id));
+        dispatch(removeProduct(id));
     };
 
     return (
         <div className="product-preview">
-            <div className="container">
-                <img src={imageSrc} alt={product.name} />
-                <div className="content">
-                    <div>
-                        <Link to={productLink} className="title link">
-                            {product.name}
-                        </Link>
-                        <NumInput
-                            amount={amount}
-                            onChange={handleAmountChange}
-                        />
-                        {price}
-                    </div>
-                </div>
-                <button className="with-svg" onClick={handleProductRemove}>
-                    <IoClose />
-                </button>
-            </div>
+            <img src={photos[0]} alt={name} />
+            <Link to={`/menu/${category}/${id}`} className="title">
+                <h4>{name}</h4>
+            </Link>
+            <NumInput amount={amount} onChange={handleAmountChange} />
+            <h4>${price * amount}</h4>
+            <button className="with-svg" onClick={handleProductRemove}>
+                <IoClose />
+            </button>
         </div>
     );
 };
