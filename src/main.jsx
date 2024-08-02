7884f9036f9be322a8f2d441c2f437099c430e9c
Kateryna Bartienieva <k7bart@gmail.com>
2024-08-02 15:52:22 +0300

    create variables.scss, create styles folder

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store/index";
-import "./index.scss";
+import "./styles/index.scss";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./AppRoutes";
