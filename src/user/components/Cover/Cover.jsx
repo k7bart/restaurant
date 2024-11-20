a861ba29f0e94df2264c5a58d1a5517a8d8bc0e2
Kateryna Bartienieva <k7bart@gmail.com>
2024-11-20 15:57:26 +0200

    refactor Cover component, move styles to module file, add tests

--- src/user/components/Cover/Cover.jsx
+++ src/user/components/Cover/Cover.jsx
@@ -1,18 +1,30 @@
+import PropTypes from "prop-types";
+
 import CoverTitles from "../cover-titles/CoverTitles";
+
+import styles from "./Cover.module.scss";
 
 function Cover({ subtitle, title, backgroundImage, text }) {
     return (
-        <div className="cover">
+        <div className={styles.cover}>
             <div
-                className="image"
+                className={styles.image}
+                data-testid="image"
                 style={{ backgroundImage: `url(${backgroundImage})` }}
             ></div>
-            <div className="filter"></div>
-            <div className="content">
-                <CoverTitles title={title} subtitle={subtitle} text={text} />
-            </div>
+
+            <div className={styles.filter}></div>
+
+            <CoverTitles title={title} subtitle={subtitle} text={text} />
         </div>
     );
 }
+
+Cover.propTypes = {
+    subtitle: PropTypes.string,
+    title: PropTypes.string,
+    backgroundImage: PropTypes.string.isRequired,
+    text: PropTypes.string,
+};
 
 export default Cover;
