a35cf2aca2ba3b647cd9b9262aa2f68561b9b521
Kateryna Bartienieva <108666032+k7bart@users.noreply.github.com>
2025-07-20 00:17:38 +0300

    add loader

---
+++ src/common/components/loader/Loader.jsx
@@ -0,0 +1,11 @@
+import styles from "./Loader.module.scss";
+
+const Loader = () => (
+    <div className={styles.loader}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+);
+export default Loader;
