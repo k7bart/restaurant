044a24ab4aba56cf0acd50084bde4768fd33c584
Kateryna Bartienieva <k7bart@gmail.com>
2025-08-26 11:35:44 +0300

    minor fix

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,3 @@
-const ACTIVITY_EVENTS = ["mousemove", "keydown", "scroll", "touchstart"];
+export const ACTIVITY_EVENTS = ["mousemove", "keydown", "scroll", "touchstart"];
 
-const INACTIVITY_TIME = 30000; // 30 seconds
-
-export { ACTIVITY_EVENTS, INACTIVITY_TIME };
+export const INACTIVITY_TIME = 30000; // 30 seconds
