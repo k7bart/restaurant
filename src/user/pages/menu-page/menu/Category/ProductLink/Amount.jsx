0116788129d7b74b2c035bab09913f14574b4011
Kateryna Bartienieva <k7bart@gmail.com>
2025-08-26 14:07:03 +0300

    refactor: move amount state from ProductLink to Amount component

--- src/user/pages/menu-page/menu/Category/ProductLink/Amount.jsx
+++ src/user/pages/menu-page/menu/Category/ProductLink/Amount.jsx
@@ -1,16 +1,38 @@
 import PropTypes from "prop-types";
+import classNames from "classnames";
+import { useProductInCart } from "../../../../../../hooks/useProductInCart";
 import NumInput from "../../../../../components/Inputs/NumInput/NumInput";
+import styles from "./ProductLink.module.scss";
 
-const Amount = ({ amount, onChange }) => {
+const Amount = ({ product, category }) => {
+    const { amount, handleAmountChange } = useProductInCart(product, category);
+
     return (
-        <div onClick={(e) => e.preventDefault()}>
-            <NumInput amount={amount} min={0} onChange={onChange} />
+        <div
+            className={classNames(styles.amount, {
+                [styles.visible]: amount,
+            })}
+            onClick={(e) => e.preventDefault()}
+        >
+            {amount ? (
+                <div>
+                    <NumInput
+                        amount={amount}
+                        min={0}
+                        onChange={handleAmountChange}
+                    />
+                </div>
+            ) : (
+                <button onClick={() => handleAmountChange(1)}>
+                    Add to cart
+                </button>
+            )}
         </div>
     );
 };
 Amount.propTypes = {
-    amount: PropTypes.number.isRequired,
-    onChange: PropTypes.func.isRequired,
+    product: PropTypes.object.isRequired,
+    category: PropTypes.string.isRequired,
 };
 
 export default Amount;
