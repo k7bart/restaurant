b06a796a5ba7392ba295b05c03c53546529c2c89
Kateryna Bartienieva <k7bart@gmail.com>
2025-11-08 22:00:56 +0200

    Referral component migration to ts

--- src/user/pages/profile-page/referral/Referral.jsx
+++ src/user/pages/profile-page/referral/Referral.jsx
@@ -1,54 +1,41 @@
 import { useState } from "react";
+import { useMe } from "../../../../hooks/useMe";
 import { IoCopyOutline } from "react-icons/io5";
-
-import PropTypes from "prop-types";
 
 import Button from "../../../../common/components/buttons/Button/Button";
 import Text from "../../../components/Text/Text";
 
 import styles from "./Referral.module.scss";
 
-const Referral = ({ id }) => {
-    const REFERRAL_LINK = "https://bart.ua/?referral=" + id; // make it work
-    const PROMO_CODE = id;
+const Referral = () => {
+    const { refferalLink, referralPromoCode } = useMe();
+    const [copiedItem, setCopiedItem] = useState(null);
 
-    const [copiedReferral, setCopiedReferral] = useState(false);
-    const [copiedPromo, setCopiedPromo] = useState(false);
+    const copyItems = [
+        { id: "referral", text: refferalLink, size: "large" },
+        { id: "promo", text: referralPromoCode, size: "small" },
+    ];
 
-    const copyReferralLink = () => {
-        navigator.clipboard.writeText(REFERRAL_LINK);
-        setCopiedReferral(true);
-        setCopiedPromo(false);
-        setTimeout(() => setCopiedReferral(false), 3000);
-    };
-
-    const copyPromoCode = () => {
-        navigator.clipboard.writeText(PROMO_CODE);
-        setCopiedPromo(true);
-        setCopiedReferral(false);
-        setTimeout(() => setCopiedPromo(false), 3000);
+    const handleCopy = (text, id) => {
+        navigator.clipboard.writeText(text);
+        setCopiedItem(id);
+        setTimeout(() => setCopiedItem(null), 3000);
     };
 
     return (
         <section className={styles.referral}>
             <div>
-                <Button
-                    size="large"
-                    color="transparent"
-                    onClick={copyReferralLink}
-                >
-                    {copiedReferral ? "Copied" : REFERRAL_LINK}
-                    <IoCopyOutline />
-                </Button>
-
-                <Button
-                    size="small"
-                    color="transparent"
-                    onClick={copyPromoCode}
-                >
-                    {copiedPromo ? "Copied" : PROMO_CODE}
-                    <IoCopyOutline />
-                </Button>
+                {copyItems.map((item) => (
+                    <Button
+                        key={item.id}
+                        size={item.size}
+                        color="transparent"
+                        onClick={() => handleCopy(item.text, item.id)}
+                    >
+                        {copiedItem === item.id ? "Copied" : item.text}
+                        <IoCopyOutline />
+                    </Button>
+                ))}
             </div>
 
             <Text align="center" size="medium">
@@ -57,10 +44,6 @@ const Referral = ({ id }) => {
             </Text>
         </section>
     );
-};
-
-Referral.propTypes = {
-    id: PropTypes.number.isRequired,
 };
 
 export default Referral;
