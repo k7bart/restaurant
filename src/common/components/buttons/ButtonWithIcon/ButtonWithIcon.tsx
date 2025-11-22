6864281a0371460324e7a21f3818a88e546ee9df
Kateryna Bartienieva <k7bart@gmail.com>
2025-11-22 17:11:42 +0200

    ButtonWithIcon migration to ts

---
+++ src/common/components/buttons/ButtonWithIcon/ButtonWithIcon.tsx
@@ -0,0 +1,39 @@
+import { type ReactNode, type MouseEvent } from "react";
+import classNames from "classnames";
+
+import styles from "./ButtonWithIcon.module.scss";
+
+type Props = {
+    className?: string;
+    icon: ReactNode;
+    onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
+    type?: "button" | "submit" | "reset";
+    disabled?: boolean;
+    ariaLabel?: string;
+    title?: string;
+};
+
+const ButtonWithIcon = ({
+    className,
+    icon,
+    onClick,
+    type = "button",
+    disabled = false,
+    ariaLabel,
+    title,
+}: Props) => {
+    return (
+        <button
+            className={classNames(styles.withIcon, className)}
+            onClick={onClick}
+            type={type}
+            disabled={disabled}
+            aria-label={ariaLabel}
+            title={title}
+        >
+            {icon}
+        </button>
+    );
+};
+
+export default ButtonWithIcon;
