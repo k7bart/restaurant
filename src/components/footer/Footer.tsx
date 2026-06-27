e15449aa8fd55b4422fe32992698f46cd7014745
Kateryna Bartienieva <k7bart@gmail.com>
2026-06-27 21:36:34 +0300

    refactor(pages): remove duplicate user page and footer copies

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -26,46 +26,38 @@ const NAV_LINKS: ReactNode = LINKS.map(({ to, text }) => (
     </CustomNavLink>
 ));
 
-const Footer = () => {
-    return (
-        <footer className={styles.footer}>
-            <div className="container">
-                <Logo />
+const Footer = () => (
+    <footer className={styles.footer}>
+        <div className="container">
+            <Logo />
 
-                <ExternalLinkComponent
-                    href="tel:+380630000000"
-                    className="link"
-                >
-                    <FaPhone />
-                    {"+380 (63) 00 00 000"}
-                </ExternalLinkComponent>
-                <ExternalLinkComponent
-                    href="mailto:b.art@gmail.com"
-                    className="link"
-                >
-                    <IoMail />
-                    b.art@gmail.com
-                </ExternalLinkComponent>
+            <ExternalLinkComponent href="tel:+380630000000">
+                <FaPhone />
+                {"+380 (63) 00 00 000"}
+            </ExternalLinkComponent>
+            <ExternalLinkComponent href="mailto:b.art@gmail.com">
+                <IoMail />
+                b.art@gmail.com
+            </ExternalLinkComponent>
 
-                <CustomLink to="https://www.linkedin.com/in/kateryna-bartienieva/">
-                    By Kateryna Bartienieva
-                </CustomLink>
-            </div>
+            <CustomLink to="https://www.linkedin.com/in/kateryna-bartienieva/">
+                By Kateryna Bartienieva
+            </CustomLink>
+        </div>
 
-            <div className="container">{NAV_LINKS}</div>
+        <div className="container">{NAV_LINKS}</div>
 
-            <div>
-                <CustomLink to="https://www.instagram.com" target="_blank">
-                    <FaInstagram />
-                    @b.art
-                </CustomLink>
-                <CustomLink to="https://www.facebook.com" target="_blank">
-                    <FaFacebook />
-                    b.art
-                </CustomLink>
-            </div>
-        </footer>
-    );
-};
+        <div>
+            <CustomLink to="https://www.instagram.com" target="_blank">
+                <FaInstagram />
+                @b.art
+            </CustomLink>
+            <CustomLink to="https://www.facebook.com" target="_blank">
+                <FaFacebook />
+                b.art
+            </CustomLink>
+        </div>
+    </footer>
+);
 
 export default Footer;
